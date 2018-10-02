964e5bdef15be1bd7fea1f0998da3c535ecf0b5d
Daniel Lima <daniel.echuaca.lima@gmail.com>
2018-10-02 14:47:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo (name){
 function happyHolidayTo (holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown (20, "Mother's Day"){
+function holidayCountdown (days, holiday){
   return `It's ${days} days until ${holiday}!`
 }
